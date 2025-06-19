3d6d5611be20d8e756a6425eb3dc2739b6cc125d
KarolainReyes <reyeskarol0412@gmail.com>
2025-06-19 18:56:14 -0500

    feat: :sparkles: Implement automatic slider functionality with pause on hover; update HTML structure and CSS for improved layout

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,26 @@
 
 //Funcionalidad para el slider//
 
+const slider = document.querySelectorAll ('.slider');
+const slides = document.querySelectorAll('.slider img');
+const totalSlides = slides.length;
+let currentSlide = 0;
+let intevalId;
+
+function startSlider () {
+    intevalId = setInterval(()=> {
+        currentSlide = (currentSlide + 1) % totalSlides
+        window.location.hash = `#slide-${currentSlide + 1}`;
+    }, 4000);
+}
+
+function stopSlider () {
+    clearInterval(intevalId);
+}
+
+//inicia el slider automatico
+startSlider ();
+
+//pausa con hover
+slider.addEventListener('mouseenter', stopSlider);
+slider.addEventListener('mouseleave', startSlider);
